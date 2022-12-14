ae68ac3076bd0184cab8c7b108025e31268a5b81
mobus <sv0220@163.com>
2022-12-14 21:58:20 +0800

    impr: add second cache to avoid first cache extend too fast too big

--- ethdb/mdbx/kv.go
+++ ethdb/mdbx/kv.go
@@ -14,7 +14,15 @@ func (d *MdbxDB) Set(dbi string, k []byte, v []byte) error {
 	buf.Reset()
 	buf.WriteString(dbi)
 	buf.Write(k)
-	d.cache.Put(buf.Bytes(), v)
+	if dbi == ethdb.AssistDBI {
+		nv, ok := d.asist[buf.String()]
+		if ok {
+			nv.Val = v
+		}
+		d.asist[buf.String()] = &NewValue{Dbi: dbi, Val: v}
+	} else {
+		d.cache.Put(buf.Bytes(), v)
+	}
 	strbuf.Put(buf)
 	return nil
 }
@@ -27,10 +35,23 @@ func (d *MdbxDB) Get(dbi string, k []byte) ([]byte, bool, error) {
 	buf.Write(k)
 
 	// query from cache
-	nvs, err := d.cache.Get(buf.Bytes())
-	strbuf.Put(buf)
-	if err == nil {
-		return nvs, true, nil
+	if dbi == ethdb.AssistDBI {
+		nv, ok := d.asist[buf.String()]
+		if ok {
+			return nv.Val, true, nil
+		}
+	} else {
+		nvs, err := d.cache.Get(buf.Bytes())
+		strbuf.Put(buf)
+		if err == nil {
+			return nvs, true, nil
+		}
+
+		nvs, err = d.bkCache.Get(buf.Bytes())
+		strbuf.Put(buf)
+		if err == nil {
+			return nvs, true, nil
+		}
 	}
 
 	var (
@@ -76,6 +97,11 @@ func (d *MdbxDB) Close() error {
 	for iter.Next() {
 		tx.Put(d.dbi[helper.B2S(iter.key[:4])], iter.key[4:], iter.value, 0)
 	}
+
+	for k, v := range d.asist {
+		tx.Put(d.dbi[v.Dbi], helper.S2B(k)[4:], v.Val, 0)
+	}
+
 	tx.Commit()
 	runtime.UnlockOSThread()
 	iter.Release()
--- ethdb/mdbx/mdbx.go
+++ ethdb/mdbx/mdbx.go
@@ -2,45 +2,29 @@ package mdbx
 
 import (
 	"bytes"
-	"encoding/binary"
 	"runtime"
 	"sync"
 	"time"
 
+	"github.com/hashicorp/go-hclog"
 	"github.com/sunvim/dogesyncer/ethdb"
 	"github.com/sunvim/dogesyncer/helper"
 	"github.com/torquem-ch/mdbx-go/mdbx"
 )
 
 type NewValue struct {
-	life int64
-	Val  []byte
-}
-
-func (nv *NewValue) Marshal() []byte {
-	rs := make([]byte, 8)
-	binary.BigEndian.PutUint64(rs, uint64(nv.life))
-	rs = append(rs, nv.Val...)
-	return rs
-}
-
-func (nv *NewValue) Unmarshal(input []byte) error {
-	life := binary.BigEndian.Uint64(input[:8])
-	nv.life = int64(life)
-	nv.Val = input[8:]
-	return nil
-}
-
-func (nv *NewValue) Reset() {
-	nv.life = 0
-	nv.Val = nil
+	Dbi string
+	Val []byte
 }
 
 type MdbxDB struct {
-	path  string
-	env   *mdbx.Env
-	cache *MemDB
-	dbi   map[string]mdbx.DBI
+	logger  hclog.Logger
+	path    string
+	env     *mdbx.Env
+	asist   map[string]*NewValue
+	cache   *MemDB
+	bkCache *MemDB
+	dbi     map[string]mdbx.DBI
 }
 
 var (
@@ -157,6 +141,8 @@ func NewMDBX(path string) *MdbxDB {
 	})
 
 	d.cache = New(cacheSize)
+	d.bkCache = New(cacheSize)
+	d.asist = make(map[string]*NewValue)
 
 	go d.syncPeriod()
 
@@ -164,36 +150,28 @@ func NewMDBX(path string) *MdbxDB {
 }
 
 func (d *MdbxDB) syncPeriod() {
-	tick := time.Tick(2 * time.Minute)
+	tick := time.Tick(30 * time.Second)
 	for range tick {
-
-		var keys [][]byte
+		d.cache, d.bkCache = d.bkCache, d.cache
 
 		runtime.LockOSThread()
 		tx, err := d.env.BeginTxn(nil, 0)
 		if err != nil {
 			panic(err)
 		}
-		iter := d.cache.NewIterator(nil)
+		iter := d.bkCache.NewIterator(nil)
 		for iter.Next() {
 			dbiName := helper.B2S(iter.key[:4])
 			_, err = tx.Get(d.dbi[dbiName], iter.key[4:])
 			if err == nil {
-				keys = append(keys, iter.key[4:])
 				continue
 			}
 			tx.Put(d.dbi[dbiName], iter.key[4:], iter.value, 0)
 		}
-
 		tx.Commit()
 		runtime.UnlockOSThread()
 		iter.Release()
 
-		for _, key := range keys {
-			d.cache.Delete(key)
-			runtime.Gosched()
-		}
-
-		keys = nil
+		d.bkCache.Reset()
 	}
 }
